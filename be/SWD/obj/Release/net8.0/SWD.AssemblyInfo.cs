9dadcd4ae724a5450ea6f0bd04524639698739c3
nle75234 <nle75234@gmail.com>
2024-03-01 20:53:59 +0700

    change VNPay controller logic, change solution file location again

--- be/SWD/obj/Release/net8.0/SWD.AssemblyInfo.cs
+++ be/SWD/obj/Release/net8.0/SWD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf5a758f79507d5bef9186977884c8b299c9bfca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+152f50e0f00339240528cb7f8bba4ccb9dffb46b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
