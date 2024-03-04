0b7149d19c7568db28fc8dbc1595d987e62d70ec
nle75234 <nle75234@gmail.com>
2024-03-04 20:49:01 +0700

    Change VNPay parameter

--- be/Services/Interface/IRequestService.cs
+++ be/Services/Interface/IRequestService.cs
@@ -1,12 +1,13 @@
 ﻿using Repositories.ModelView;
+using static Repositories.ModelView.CartView;
 
 namespace Services.Interface
 {
     public interface IRequestService
     {
-        public Task<string> Payment(string requestID, int TotalPrice);
+        public Task<string> Payment(string requestID, AddCartView[] cartViews);
         public Task<object> GetAllRequest(int pageIndex, int pageSize, bool isAsc, string sortField, string searchValue, string searchField);
-        public Task<string> AddPendingRequest(string _id, int totalPrice);
+        public Task<string> AddPendingRequest(string _id, AddCartView[] cartViews);
         public Task<string> UpdateStatusRequest(string _id);
         public Task<string> DeleteRequest(string _id);
         public Task<string> CheckPayment(string url);
