2c70be77948abd5a933fc9a74b8ddc0466012e85
nle75234 <nle75234@gmail.com>
2024-03-04 20:57:37 +0700

    .

--- be/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ be/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef3a38da09e2bc929376d1acf34e9cb1d2ebb9ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b7149d19c7568db28fc8dbc1595d987e62d70ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
